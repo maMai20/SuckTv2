694b06f1592b4590b08aa827b8f0023fb0c62871
maMai20 <xboxrrr97@gmail.com>
2025-08-13 07:25:08 -0700

    เปลี่ยนหน้าแรกเป็น Home

--- Frontend/suckt/src/app/page.tsx
+++ Frontend/suckt/src/app/page.tsx
@@ -1,9 +1,5 @@
-'use client';
+import { redirect } from "next/navigation";
 
-export default function HomePage() {
-  return (
-    <>
-    hi
-    </>
-  );
+export default function Page() {
+  redirect("/home");
 }
